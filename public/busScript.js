df90612f599e5458c31ee1c45cb0e2cbe2117512
Mikko Kotola <mikko.kotola@sympa.com>
2020-01-25 13:51:44 +0200

    Use HTTPS and user geolocation

--- public/busScript.js
+++ public/busScript.js
@@ -1,16 +1,25 @@
-var updateIntervalInMs = 5000
+const urlBase = 'https://localhost:3001';
+const url = urlBase + '/buses';
+const updateIntervalInMs = 5000
+var map;
+var busMarkers = [];
+var ownPositionMarker;
 var icon;
-var mapInitialCenterPos = {
+const mapDefaultCenter = {
     lat: 60.976651,
     lng: 25.666292
-}
-var map;
-var markers = [];
+};
 
-function initMap() {
+const geo_options = {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 27000
+};
+
+function createMap(mapCenter) {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: mapInitialCenterPos,
-        zoom: 13
+        center: mapCenter,
+        zoom: 15
     });
 
     icon = {
@@ -19,33 +28,63 @@ function initMap() {
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(0, 0)
     };
-    updateMap();
 }
-function updateMap() {
-    const url = 'http://localhost:3001/buses';
 
+function createMapWithGeoLocation(position) {
+    mapCenter = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    };
+    createMap(mapCenter);
+    updateMap();
+    var wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
+}
+
+function createMapWithDefaultLocation() {
+    createMap(mapDefaultCenter);
+    updateMap();
+    var wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
+}
+
+function initMap() {
+    navigator.geolocation.getCurrentPosition(createMapWithGeoLocation, createMapWithDefaultLocation, geo_options);
+}
+
+function updateMap() {
     fetch(url)
         .then(function (response) {
             response.json()
                 .then(function (buses) {
-                    tmpMarkers = markers;
+                    tmpMarkers = busMarkers;
                     deleteMarkers();
                     console.log('Got response, processing buses');
                     buses.forEach(bus => {
                         // Only make markers for Lahti area - API seems to be giving data for wrong city occasionally
-                        if (bus.vehicle.position.latitude > 60.60 && bus.vehicle.position.latitude < 61.40 && bus.vehicle.position.longitude > 24.70 && bus.vehicle.position.longitude < 26.50) {
-                            makeMarker(bus.vehicle.position.latitude, bus.vehicle.position.longitude, bus.vehicle.trip.routeName);
+                        if (isWithinGeoBox(bus)) {
+                            makeBusMarker(bus.vehicle.position.latitude, bus.vehicle.position.longitude, bus.vehicle.trip.routeName);
                         }
                     });
-                    if (markers.length == 0) {
-                        markers = tmpMarkers;
+                    if (busMarkers.length == 0) {
+                        busMarkers = tmpMarkers;
                     }
                     showMarkers();
                 });
         });
 }
 
-function makeMarker(lat, lon, txt) {
+function makeOwnPositionMarker(lat, lon) {
+    console.log('Making own position marker');
+    if (ownPositionMarker) {
+        ownPositionMarker.setMap(null);
+    }
+    ownPositionMarker = new google.maps.Marker({
+        position: { lat: lat, lng: lon },
+        map: map        
+    });
+    ownPositionMarker.setMap(map);
+}
+
+function makeBusMarker(lat, lon, txt) {
     console.log('Making marker with text ' + txt);
     var marker = new google.maps.Marker({
         position: { lat: lat, lng: lon },
@@ -54,15 +93,15 @@ function makeMarker(lat, lon, txt) {
         label: { color: '#000000', fontWeight: 'bold', fontSize: '16px', text: txt }
     });
     console.log('Pushing marker with latlon ' + lat + ', ' + lon);
-    markers.push(marker);
+    busMarkers.push(marker);
 }
 
 // Sets the map on all markers in the array.
 function setMapOnAll(mapp) {
-    console.log('In setMapOnAll, markers length ' + markers.length);
-    for (var i = 0; i < markers.length; i++) {
-        console.log(markers[i]);
-        markers[i].setMap(mapp);
+    console.log('In setMapOnAll, markers length ' + busMarkers.length);
+    for (var i = 0; i < busMarkers.length; i++) {
+        console.log(busMarkers[i]);
+        busMarkers[i].setMap(mapp);
     }
 }
 
@@ -81,7 +120,23 @@ function showMarkers() {
 // Deletes all markers in the array by removing references to them.
 function deleteMarkers() {
     clearMarkers();
-    markers = [];
+    busMarkers = [];
+}
+
+function isWithinGeoBox(bus) {
+    return bus.vehicle.position.latitude > 60.60 && bus.vehicle.position.latitude < 61.40 && bus.vehicle.position.longitude > 24.70 && bus.vehicle.position.longitude < 26.50
+}
+
+function geo_success(position) {
+    makeOwnPositionMarker(position.coords.latitude, position.coords.longitude);
+    map.panTo({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    });
+}
+
+function geo_error() {
+    alert("No geolocation available for user, centering Map in downtown Lahti");
 }
 
 setInterval(updateMap, updateIntervalInMs);
